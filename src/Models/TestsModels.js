fa500ffa90c050390fef0bf020ba253c6b2c5998
michele.gusmao <michele.gusmao@fiap.com.br>
2025-01-31 17:46:26 -0300

    feat: add rota tests

--- src/Models/TestsModels.js
+++ src/Models/TestsModels.js
@@ -2,39 +2,46 @@ import mongoose from 'mongoose';
 
 const Test = mongoose.model('Test', testSchema);
 
-const updateTestField = async (id, field, data, updateIP = true) => {
-  try {
-    const updateData = {
-      [field]: data,
-    };
-
-    if (updateIP) {
-      updateData.updatedAtIP = req.userIp || 'N/A';
-    }
-
-    await Test.updateOne({ _id: id }, updateData);
-  } catch (error) {
-    throw new Error('Erro ao atualizar prova: ' + error.message);
-  }
-};
-
 const createTest = async (testData, user) => {
   try {
-    if (user) {
-      testData.professor = user._id;
-    }
-    const newTest = await Test.create(testData);
+
+    const newTest = await Test.create({
+      ...testData,
+      subject: testData.subjectId,
+      topic: testData.topic,
+      grade: testData.gradeId,
+      accessibility: testData.accessibility || false,
+      createdBy: user._id,
+    });
     return newTest;
   } catch (error) {
     throw new Error('Erro ao criar prova: ' + error.message);
   }
 };
 
+const updateTest = async (id, test) => {
+    try {
+      await Test.findByIdAndUpdate(
+        id,
+        {
+          ...test,
+          subject: test.subjectId,
+          grade: test.gradeId,
+          difficulty: capitalize(test.difficulty),
+        },
+        { new: true },
+      );
+    } catch (error) {
+      throw new Error('Erro ao atualizar prova: ' + error.message);
+    }
+  };
+
 const getTestById = async (id) => {
   try {
     const test = await Test.findById(id)
       .populate('questions')
-      .populate('professor');
+      .populate('subject', 'name')
+      .populate('grade', 'name');
     if (!test) {
       throw new Error('Prova não encontrada.');
     }
@@ -44,38 +51,46 @@ const getTestById = async (id) => {
   }
 };
 
-const getAllActiveTests = async () => {
+const getAllTests = async (userId, page, limit) => {
   try {
-    const tests = await Test.find({ status: 'active' });
-    return tests;
+    const skip = (page - 1) * limit;
+
+    const totalTests = await Test.countDocuments({
+      createdBy: userId,
+    });
+
+    const tests = await Test.find({ createdBy: userId })
+      .populate('questions')
+      .populate('createdBy', 'personalInfo.name personalInfo.email')
+      .populate('subject', 'name')
+      .populate('grade', 'name')
+      .skip(skip)
+      .limit(limit);
+    
+    const totalPages = Math.ceil(totalTests / limit);
+
+     return {
+      tests,
+      totalTests,
+      totalPages,
+    };
   } catch (error) {
-    throw new Error('Erro ao buscar provas ativas: ' + error.message);
+    throw new Error('Erro ao buscar provas: ' + error.message);
   }
 };
 
-const getTestsByProfessor = async (professorId) => {
-  try {
-    const tests = await Test.find({ professor: professorId });
-    return tests;
-  } catch (error) {
-    throw new Error('Erro ao buscar provas por professor: ' + error.message);
-  }
-};
-
-const getTestsByStudentGrade = async (grade) => {
-  try {
-    const tests = await Test.find({ grade });
-    return tests;
-  } catch (error) {
-    throw new Error('Erro ao buscar provas por turma: ' + error.message);
-  }
-};
+const deleteTest = async (id) => {
+    try {
+      await Test.findByIdAndDelete(id);
+    } catch (error) {
+      throw new Error('Erro ao deletar prova: ' + error.message);
+    }
+  };
 
 export default {
-  updateTestField,
+  updateTest,
   createTest,
   getTestById,
-  getAllActiveTests,
-  getTestsByProfessor,
-  getTestsByStudentGrade,
+  getAllTests,
+  deleteTest,
 };
---
+++ src/Routers/TestsRouters.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import TestController from '../Controllers/TestController.js';
+import UserMiddleware from '../Middlewares/UsersMiddlewares.js';
+
+const router = express.Router();
+
+router.get(
+    '/allTests', 
+    UserMiddleware.authenticate, 
+    TestController.getAllTests
+);
+
+router.get(
+    '/:id', 
+    UserMiddleware.authenticate, 
+    TestController.getTestById
+);
+
+router.post(
+    '/create', 
+    UserMiddleware.authenticate, 
+    TestController.createTest
+);
+
+router.put(
+    '/:id', 
+    UserMiddleware.authenticate, 
+    TestController.updateTest
+);
+
+router.delete(
+    '/:id', 
+    UserMiddleware.authenticate, 
+    TestController.deleteTest
+);
+
+export default router;
